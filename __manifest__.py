a08f0f56f1c2a2d7d73f078b38aa5a5469fe0316
fededmarengo <48831675+fededmarengo@users.noreply.github.com>
2021-07-03 11:49:54 -0300

    V14

---
+++ __manifest__.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "my_module",
+
+    'summary': """
+        Short (1 phrase/line) summary of the module's purpose, used as
+        subtitle on modules listing or apps.openerp.com""",
+
+    'description': """
+        Long description of module's purpose
+    """,
+
+    'author': "My Company",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],    
+	
+	'installable': True,
+	
+	# hooks: https://www.cybrosys.com/blog/how-to-use-hooks-in-odoo-development
+
+	# 'post_init_hook': 'function name on __init__.py',
+	# 'post_init_hook': '';
+	# 'uninstall_hook': '';
+	# 'post_load': '';
+	
+}
